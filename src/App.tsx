6f1ab959149d3af34be9b1e81fbae15366fa2146
Rodrigo <rod_novaes@hotmail.com>
2025-10-22 12:54:17 +0200

    Add the Routes and components to the App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,30 @@
-import Style from "./App.module.scss";
+import { useState } from "react";
+import { Route, Routes } from "react-router";
+import Header from "./components/Header/Header";
+import HomePage from "./pages/home/home";
+import ProductDetailsPage from "./pages/product-details/product-details";
+import CartPage from "./pages/cart/cart";
+import SearchBar from "./components/SearchBar/SearchBar";
 
 function App() {
-  return <div className={Style.App}>Hello</div>;
+  const [searchTerm, setSearchTerm] = useState("");
+
+  return (
+    <>
+      <Header />
+      <SearchBar
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+      <main>
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/details/:id' element={<ProductDetailsPage />} />
+          <Route path='/cart' element={<CartPage />} />
+        </Routes>
+      </main>
+    </>
+  );
 }
 
 export default App;
