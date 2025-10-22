8881e1bbf8d2641dffd6f3106df044552b89e5cc
Rodrigo <rod_novaes@hotmail.com>
2025-10-22 21:29:48 +0200

    Wrap the App component with the context providers

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,17 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router";
+import { ProductProvider } from "./context/ProductContext.tsx";
+import { ProductDetailsProvider } from "./context/ProductDetailsContext.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <ProductProvider>
+        <ProductDetailsProvider>
+          <App />
+        </ProductDetailsProvider>
+      </ProductProvider>
     </BrowserRouter>
   </StrictMode>
 );
