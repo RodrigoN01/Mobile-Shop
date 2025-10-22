0a01f75a8a133b0db4b8630e81c599fd17339a1f
Rodrigo <rod_novaes@hotmail.com>
2025-10-22 21:31:27 +0200

    Create the ProductCard component

---
+++ src/components/ProductCard/ProductCard.tsx
@@ -0,0 +1,24 @@
+import Styles from "./ProductCard.module.scss";
+import type { Product } from "../../types";
+
+type ProductCardProps = Omit<Product, "id">;
+
+const ProductCard = ({ model, price, imgUrl, brand }: ProductCardProps) => {
+  return (
+    <li className={Styles.ProductCard}>
+      <img
+        className={Styles.ProductCard__image}
+        src={imgUrl}
+        alt={model}
+        width={220}
+        height={220}
+      />
+      <p className={Styles.ProductCard__brand}>{brand}</p>
+      <div className={Styles.ProductCard__info}>
+        <p>{model}</p>
+        {price && <p>${price}</p>}
+      </div>
+    </li>
+  );
+};
+export default ProductCard;
